f80769a0bfbe134f91493cf73bd376544c6e70a9
Yohan Silva <yohanps.dev@gmail.com>
2023-02-08 17:16:04 -0300

    feat: create save user

--- src/main/java/br/com/dicasdeumdev/api/resources/UserResource.java
+++ src/main/java/br/com/dicasdeumdev/api/resources/UserResource.java
@@ -6,10 +6,12 @@ import br.com.dicasdeumdev.api.services.UserService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/user")
@@ -24,5 +26,21 @@ public class UserResource {
     @GetMapping("/{id}")
     public ResponseEntity<UserDTO> findById(@PathVariable Integer id) {
         return ResponseEntity.ok().body(mapper.map(service.findById(id), UserDTO.class));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<UserDTO>> findAll() {
+        return ResponseEntity.ok().body(service.findAll()
+                .stream()
+                .map(c -> mapper.map(c , UserDTO.class))
+                .collect(Collectors.toList()));
+    }
+
+    @PostMapping
+    public ResponseEntity<UserDTO> create(@RequestBody UserDTO userDTO) {
+        URI uri=ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(service.create(userDTO).getId()).toUri();
+
+        return ResponseEntity.created(uri).build();
     }
 }
--- src/main/java/br/com/dicasdeumdev/api/services/impl/UserServiceImpl.java
+++ src/main/java/br/com/dicasdeumdev/api/services/impl/UserServiceImpl.java
@@ -1,12 +1,15 @@
 package br.com.dicasdeumdev.api.services.impl;
 
 import br.com.dicasdeumdev.api.domain.User;
+import br.com.dicasdeumdev.api.domain.dto.UserDTO;
 import br.com.dicasdeumdev.api.exeptions.ObjectNotFoundException;
 import br.com.dicasdeumdev.api.repositories.UserRepository;
 import br.com.dicasdeumdev.api.services.UserService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -15,9 +18,22 @@ public class UserServiceImpl implements UserService {
     @Autowired
     private UserRepository repository;
 
+    @Autowired
+    private ModelMapper modelMapper;
+
     @Override
     public User findById(Integer id) {
         Optional<User> obj = repository.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+    @Override
+    public List<User> findAll() {
+        return repository.findAll();
+    }
+
+    @Override
+    public User create(UserDTO userDTO) {
+        return repository.save(modelMapper.map(userDTO, User.class));
     }
 }
