fc8363190f7d6e52692867a464b70a4bff27db4e
Yohan Silva <yohanps.dev@gmail.com>
2023-02-07 23:33:19 -0300

    feat: create custom error

--- src/main/java/br/com/dicasdeumdev/api/services/impl/UserServiceImpl.java
+++ src/main/java/br/com/dicasdeumdev/api/services/impl/UserServiceImpl.java
@@ -1,9 +1,9 @@
 package br.com.dicasdeumdev.api.services.impl;
 
 import br.com.dicasdeumdev.api.domain.User;
+import br.com.dicasdeumdev.api.exeptions.ObjectNotFoundException;
 import br.com.dicasdeumdev.api.repositories.UserRepository;
 import br.com.dicasdeumdev.api.services.UserService;
-import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -18,6 +18,6 @@ public class UserServiceImpl implements UserService {
     @Override
     public User findById(Integer id) {
         Optional<User> obj = repository.findById(id);
-        return obj.orElse(null);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
     }
 }
